debbb449bb007003916fd547e1f7d9cf13d1e1ec
Tu Dinh <1257909+dinhngtu@users.noreply.github.com>
2024-09-08 18:49:53 +0200

    tweak doit script

--- doit.ps1
+++ doit.ps1
@@ -5,8 +5,17 @@ param (
     [Parameter()]
     [switch]$Linear,
     [Parameter()]
+    $LogPath,
+    [Parameter()]
     $OutFile = "TCGlog.json"
 )
 
+$ErrorActionPreference = "Break"
+
 import-Module "$PSScriptRoot\TCGLogTools.psd1" -Force
-ConvertTo-TCGEventLog -LogBytes (Get-TCGLogContent -LogType SRTMCurrent) -MinimizedX509CertInfo -GatherSBCP:$Gather -GatherDevice:$Gather -Linear:$Linear | ConvertTo-Json -Depth 99 | Out-File $OutFile
+if ($LogPath) {
+    $LogBytes = [IO.File]::ReadAllBytes($LogPath)
+} else {
+    $LogBytes = Get-TCGLogContent -LogType SRTMCurrent
+}
+ConvertTo-TCGEventLog -LogBytes $LogBytes -MinimizedX509CertInfo -GatherSBCP:$Gather -GatherDevice:$Gather -Linear:$Linear | ConvertTo-Json -Depth 99 | Out-File $OutFile
